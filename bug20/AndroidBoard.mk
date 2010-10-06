775fc70ef5fbbef8e0f2652a6641ed8fb0dc19a9
Robert Schuster <gnurobert@gmail.com>
2010-10-06 14:12:06 +0200

    * added boot logo and its source  * added boot logo copying instructions to makefile

--- bug20/AndroidBoard.mk
+++ bug20/AndroidBoard.mk
@@ -1,6 +1,10 @@
-# make file for new hardware  from 
-#
 LOCAL_PATH := $(call my-dir)
+
+# Installs boot logo
+file := $(TARGET_ROOT_OUT)/initlogo.rle
+ALL_PREBUILT += $(file)
+$(file) : $(LOCAL_PATH)/initlogo.rle | $(ACP)
+        $(transform-prebuilt-to-target)
 
 #
 # this is here to use the pre-built kernel
