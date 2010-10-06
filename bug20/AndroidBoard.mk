4618f46ab20fe1ce0cba4fdbde8b8b8d76182bd3
Robert Schuster <gnurobert@gmail.com>
2010-10-06 15:02:39 +0200

    - cleaned up AndroidBoard.mk

--- bug20/AndroidBoard.mk
+++ bug20/AndroidBoard.mk
@@ -6,25 +6,17 @@ ALL_PREBUILT += $(file)
 $(file) : $(LOCAL_PATH)/initlogo.rle | $(ACP)
         $(transform-prebuilt-to-target)
 
+# Kernel installation - disabled for now. Kernel build is outside
+# of Android scope
 #
 # this is here to use the pre-built kernel
-ifeq ($(TARGET_PREBUILT_KERNEL),)
-	TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
-endif
+#ifeq ($(TARGET_PREBUILT_KERNEL),)
+#	TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel
+#endif
+#
+#file := $(INSTALLED_KERNEL_TARGET)
+#ALL_PREBUILT += $(file)
+#$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
+#	$(transform-prebuilt-to-target)
 
-#
-file := $(INSTALLED_KERNEL_TARGET)
-ALL_PREBUILT += $(file)
-$(file): $(TARGET_PREBUILT_KERNEL) | $(ACP)
-	$(transform-prebuilt-to-target)
-
-#
-# no boot loader, so we don't need any of that stuff..  
-#
-LOCAL_PATH := device/buglabs/bug20
-#
 include $(CLEAR_VARS)
-
-#
-# include more board specific stuff here? Such as Audio parameters.      
-#
